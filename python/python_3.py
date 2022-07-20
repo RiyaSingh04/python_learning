ef26c42b18b3002e87429ab65166a8f17e829d9c
RiyaSingh04 <105190041+RiyaSingh04@users.noreply.github.com>
2022-07-20 15:51:59 +0530

    Add files via upload

---
+++ python/python_3.py
@@ -0,0 +1,61 @@
+# # 19 july
+# # frequency
+# n = input('enter a string')
+# map_ ={}
+# for i in n:
+#     if i in map_:
+#         map_[i] += 1
+#     else:
+#         map_[i] = 1
+# print(map_) 
+
+# #upper and lower case same consider
+# n = input('enter a string')
+# n = n.lower()
+# map_ = {}
+# for i in n:
+#     if i in map_:
+#         map_[i] += 1
+#     else:
+#         map_[i] = 1
+# print(map_) 
+
+# #break
+# for i in range(5):
+#     if(i == 3):
+#         break
+#     else:
+#         print(i)
+
+# # listquestion
+# n = int(input('enter a number'))
+# lst = []
+# for i in range (n):
+#     op= input().split()
+#     if op[0] == 'push':
+#         lst.append(op[1])
+#         print(lst)
+#     elif op[0] == 'pop':
+#         lst.remove(op[1])
+#         print(lst)
+#     elif op[0] == 'stop':
+#         break
+
+# #setsquestion
+n = input('enter a string')
+map_ = ()
+for i in n:
+    if i in map_:
+        
+
+
+
+
+
+
+
+
+
+
+
+
